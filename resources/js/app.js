23a29fdcb946ce4431af36153af0c08ba9dd589c
unknown <shishir.nepal.9@gmail.com>
2020-10-14 14:32:45 +0545

    adding files

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+
+window.Vue=require('vue');
+
+
+Vue.component(
+    'articles',
+    require('./components/Articles.vue')
+);
+
+const app= new Vue({
+    el:'#app'
+});
